a6b1fd59bd397bc90c4fc107b9da899545413620
Maartendohmen <maartendohmen@gmail.com>
2017-03-07 09:43:18 +0100

    First commit (added classes)

---
+++ Game classes implement/Player.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game_classes_implement
+{
+    class Player
+    {
+        //fields
+
+        private bool powerUp;
+        private int hitPoints;
+    }
+}
